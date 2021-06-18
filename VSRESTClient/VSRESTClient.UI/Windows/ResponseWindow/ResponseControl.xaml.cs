03ce8dfaf645054e5de8dec53373eb3cbfefe5da
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2021-06-18 14:11:45 +0200

    add a ResponseWindow

---
+++ VSRESTClient/VSRESTClient.UI/Windows/ResponseWindow/ResponseControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VSRESTClient.UI.Windows.ResponseWindow
+{
+    /// <summary>
+    /// Interaction logic for ResponseControl.xaml
+    /// </summary>
+    public partial class ResponseControl : UserControl
+    {
+        public ResponseControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
